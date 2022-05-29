b56101ea909a50b3aa32dfe26da6e5ce3d30c199
brunoprado55 <bruno.prado@bemtevi.com>
2022-05-29 04:38:23 -0300

    Update install_test.ps1

--- install_test.ps1
+++ install_test.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 # Test that we can install the latest version at the default location.
 Remove-Item "~\.deno" -Recurse -Force -ErrorAction SilentlyContinue
 $env:DENO_INSTALL = ""
-$v = $null; .\install.ps1
+$ = $null; .\install.ps1
 ~\.deno\bin\deno.exe --version
 
 # Test that we can install a specific version at a custom location.
